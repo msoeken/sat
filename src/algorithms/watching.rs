b51d9592146596b634b1f9cd9f3db7e38eb013ad
Mathias Soeken <mathias.soeken@outlook.com>
2021-06-28 22:55:51 +0200

    Refactor Algorithm B.

--- src/algorithms/watching.rs
+++ src/algorithms/watching.rs
@@ -1,35 +1,67 @@
-use std::ops::Not;
-
-use crate::{Lit, Solver};
+use crate::{Lit, Solver, Var};
 
 struct WatchList {
     /// matches a clause to another clause in the linked list
-    pub link: Vec<usize>,
+    link_: Vec<usize>,
 
     /// maps literals to clause index (0 means no clause)
-    pub watch: Vec<usize>,
+    watch_: Vec<usize>,
 }
 
 impl WatchList {
     /// creates a new watch list
     pub fn new(num_vars: usize, num_clauses: usize) -> Self {
         Self {
-            link: vec![0; num_clauses + 1],
-            watch: vec![0; 2 * (num_vars + 1)],
+            link_: vec![0; num_clauses + 1],
+            watch_: vec![0; 2 * (num_vars + 1)],
         }
     }
 
     /// insert a new entry to the watch list
-    pub fn insert(&mut self, lit: &Lit, clause_index: usize) {
-        if self.watch[lit.index as usize] == 0 {
+    pub fn create_or_insert(&mut self, lit: &Lit, clause_index: usize) {
+        let w = &mut self.watch_[lit.index as usize];
+
+        if *w == 0 {
             // first watch?
-            self.watch[lit.index as usize] = clause_index;
+            *w = clause_index;
         } else {
             // new watch?
-            let w = self.watch[lit.index as usize];
-            self.watch[lit.index as usize] = clause_index;
-            self.link[clause_index] = w;
+            self.link_[clause_index] = *w;
+            *w = clause_index;
         }
+    }
+
+    /// insert a new clause being watched by lit
+    pub fn insert(&mut self, lit: &Lit, clause_index: usize) {
+        let w = &mut self.watch_[lit.index as usize];
+
+        self.link_[clause_index] = *w;
+        *w = clause_index;
+    }
+
+    /// make literal watch a specific clause (without changing linked lists)
+    pub fn watch(&mut self, lit: &Lit, clause_index: usize) {
+        self.watch_[lit.index as usize] = clause_index;
+    }
+
+    /// returns the watched clause by lit
+    pub fn watchee(&self, lit: &Lit) -> usize {
+        self.watch_[lit.index as usize]
+    }
+
+    /// returns the next clause in the linked list (or 0 if last clause)
+    pub fn link(&self, clause_index: usize) -> usize {
+        self.link_[clause_index]
+    }
+
+    /// stops watching clauses for a literal
+    pub fn remove(&mut self, lit: &Lit) {
+        self.watch_[lit.index as usize] = 0;
+    }
+
+    /// checks whether a literal is watching a clause
+    pub fn is_watching(&self, lit: &Lit) -> bool {
+        self.watch_[lit.index as usize] != 0
     }
 }
 
@@ -69,11 +101,13 @@ impl Solver for WatchingSolver {
             self.lits.push(l);
         }
 
-        self.watch_list.insert(&clause[0], self.cls_counter);
+        self.watch_list
+            .create_or_insert(&clause[0], self.cls_counter);
 
         self.cls_counter -= 1;
     }
 
+    #[allow(clippy::many_single_char_names)]
     fn solve(&mut self) -> bool {
         assert_eq!(self.cls_counter, 0);
         self.start[0] = self.lits.len();
@@ -88,13 +122,9 @@ impl Solver for WatchingSolver {
                 return true;
             }
 
-            m[d as usize] = if self.watch_list.watch[2 * d as usize] == 0
-                || self.watch_list.watch[2 * d as usize + 1] != 0
-            {
-                1
-            } else {
-                0
-            };
+            m[d as usize] = (!self.watch_list.is_watching(&Lit::pos(Var::new(d)))
+                || self.watch_list.is_watching(&Lit::neg(Var::new(d))))
+            .into();
             let mut l = Lit {
                 index: 2 * d + m[d as usize],
             };
@@ -105,20 +135,22 @@ impl Solver for WatchingSolver {
             while cont {
                 cont = false;
 
-                let mut j = self.watch_list.watch[l.not().index as usize];
+                let mut j = self.watch_list.watchee(&!l);
                 while j != 0 {
                     let i = self.start[j];
                     let ii = self.start[j - 1];
-                    let jj = self.watch_list.link[j];
+                    // remember where j links to, as we may change this later
+                    let jj = self.watch_list.link(j);
                     let mut k = i + 1;
                     while k < ii {
                         let ll = self.lits[k];
+                        // if ll has not been assigned yet...
                         if (ll.var_index() > d) || (ll.index + m[ll.var_index() as usize]) % 2 == 0
                         {
                             self.lits[i] = ll;
-                            self.lits[k] = l.not();
-                            self.watch_list.link[j] = self.watch_list.watch[ll.index as usize];
-                            self.watch_list.watch[ll.index as usize] = j;
+                            self.lits[k] = !l;
+                            // Now ll watches clause j
+                            self.watch_list.insert(&ll, j);
                             j = jj;
                             break;
                         }
@@ -128,7 +160,7 @@ impl Solver for WatchingSolver {
                         }
                     }
                     if k == ii {
-                        self.watch_list.watch[l.not().index as usize] = j;
+                        self.watch_list.watch(&!l, j);
 
                         // B5. [Try again.]
                         loop {
@@ -155,7 +187,7 @@ impl Solver for WatchingSolver {
             }
 
             // B4. [Advance.]
-            self.watch_list.watch[l.not().index as usize] = 0;
+            self.watch_list.remove(&!l);
             d += 1;
 
             // goto B2.
